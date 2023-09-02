00a79dba67a1ad2f208d0526d8c97ebcb0d43ba4
BiwashGurung <varungrg10@gmail.com>
2023-09-02 15:53:50 +0545

    changing font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Other Tailwind CSS configurations
+  
+    theme: {
+      extend: {
+        fontFamily: {
+          'carolyna-pro-black': ['Carolyna Pro Black', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
