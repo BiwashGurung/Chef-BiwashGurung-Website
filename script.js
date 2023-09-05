72f97fd322fc0898a5257b18889eae22c89f981d
BiwashGurung <varungrg10@gmail.com>
2023-09-05 09:52:53 +0545

    added a space between clear button

--- script.js
+++ script.js
@@ -70,59 +70,57 @@ document.querySelector("footer .footer-year").textContent =
 
 
 
-
-
 //OPen close clear button for add to cart
 const Cart = [];
 
-    function addToCart(itemName, itemPrice) {
-      const item = {
-        name: itemName,
-        price: itemPrice,
-      };
-      cart.push(item);
+
+  function addToCart(itemName, itemPrice) {
+    const item = {
+      name: itemName,
+      price: itemPrice,
+    };
+    cart.push(item);
+    updateCartSlider();
+  }
+
+  function clearCartItem(index) {
+    if (index >= 0 && index < cart.length) {
+      cart.splice(index, 1); // Remove the item at the specified index
       updateCartSlider();
     }
-  
-    function clearCartItem(index) {
-      if (index >= 0 && index < cart.length) {
-        cart.splice(index, 1); // Remove the item at the specified index
-        updateCartSlider();
-      }
-    }
-  
-    function closeCart() {
-      const cartSlider = document.getElementById('cartSlider');
-      cartSlider.style.transform = 'translateX(100%)'; // Close the cart slider
-    }
-  
-    function updateCartSlider() {
-      const cartSlider = document.getElementById('cartSlider');
-      const cartItemsList = document.getElementById('cartItems');
-  
-      cartItemsList.innerHTML = ''; // Clear existing cart items
-  
-      cart.forEach((item, index) => {
-        const cartItem = document.createElement('li');
-        cartItem.textContent = `${item.name} - NPR ${item.price}`;
-        
-        // Create a "Clear" button for each item
-        const clearButton = document.createElement('button');
-        clearButton.textContent = 'Clear';
-        clearButton.classList.add('text-red-500', 'hover:text-red-700');
-        clearButton.onclick = () => clearCartItem(index);
-        
-        cartItem.appendChild(clearButton);
-        cartItemsList.appendChild(cartItem);
-      });
-  
-      if (cart.length > 0) {
-        cartSlider.style.transform = 'translateX(0)'; // Show the cart slider
-      } else {
-        cartSlider.style.transform = 'translateX(100%)'; // Hide the cart slider if the cart is empty
-      }
-    }    
+  }
 
+  function closeCart() {
+    const cartSlider = document.getElementById('cartSlider');
+    cartSlider.style.transform = 'translateX(100%)'; // Close the cart slider
+  }
+
+  function updateCartSlider() {
+    const cartSlider = document.getElementById('cartSlider');
+    const cartItemsList = document.getElementById('cartItems');
+
+    cartItemsList.innerHTML = ''; // Clear existing cart items
+
+    cart.forEach((item, index) => {
+      const cartItem = document.createElement('li');
+      cartItem.innerHTML = `${item.name} - NPR ${item.price}&nbsp;&nbsp;`; // Add a non-breaking space (&nbsp;) between price and Clear button
+
+      // Create a "Clear" button for each item
+      const clearButton = document.createElement('button');
+      clearButton.textContent = 'Clear';
+      clearButton.classList.add('text-red-500', 'hover:text-red-700');
+      clearButton.onclick = () => clearCartItem(index);
+
+      cartItem.appendChild(clearButton);
+      cartItemsList.appendChild(cartItem);
+    });
+
+    if (cart.length > 0) {
+      cartSlider.style.transform = 'translateX(0)'; // Show the cart slider
+    } else {
+      cartSlider.style.transform = 'translateX(100%)'; // Hide the cart slider if the cart is empty
+    }
+  }
 
     
 
